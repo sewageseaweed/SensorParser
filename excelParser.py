fd19b0fa61e0662dd4b636821e9f17e714e5b504
sewageseaweed <csumagang@csumb.edu>
2021-09-13 12:39:36 -0700

    Finished initial GUI and functionality between parser backend with the GUI. Created linux executable but file too large to upload

--- excelParser.py
+++ excelParser.py
@@ -1,4 +1,10 @@
-# -*- coding: utf-8 -*-
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import numpy as np
+import pandas as pd
+
 """
 Created on Mon Mar  1 03:38:11 2021
 
@@ -10,188 +16,208 @@ TODO: Clean up code
 TODO: Make more efficient
 """
 
-import numpy as np
-import pandas as pd
-
-df = pd.read_csv(r"C:\Users\csumagang\Desktop\SmartWash_2020\frazier.csv", encoding= 'unicode_escape')
-df.shape
-
 # FIELD WEATHER
-renamed = []
-for i in df:
-    renamed.append(i.strip())
 
-df.columns = renamed
-list(df)
-df.shape
-list(df['Date & Time'].values)
 
-renamed[44:]
-want = renamed[44:]
-want.append('Date & Time')
-df2 = pd.DataFrame()
+def fieldWeather(path, start, csv):
+    print("HERE I AM")
+    if csv:
+        df = pd.read_csv(path, encoding='unicode_escape')
+    else:
+        df = pd.read_excel(path)
 
-for i in want:
-    df2[i] = df[i]
+    renamed = []
+    for i in df:
+        renamed.append(i.strip())
 
-print(df2[want[0]][4])
-matrix = []
-df2.replace(['--'], np.nan, inplace=True)
-for i in df2.columns:
-    matrix.append(i.split())
+    df.columns = renamed
+    list(df)
+    df.shape
+    list(df['Date & Time'].values)
 
-for i in matrix:
-    if i[1].lower() != "port":
-        i[0] += i[1]
-        del i[1]
-    print(i, len(i))
+    renamed[start:]
+    want = renamed[start:]
+    want.append('Date & Time')
+    df2 = pd.DataFrame()
 
-df3 = pd.DataFrame(columns = ['ID', 'Field ID', 'Date/Time', 'Sensor Type', 'Sensor Result', 'Unit']);
+    for i in want:
+        df2[i] = df[i]
 
-count = 0
-lol = 0
-df2_columns = list(df2.columns)
-for index, row in df2.iterrows():
-    for j in df2_columns:
-        print(lol)
-        lol+=1
-        if j == 'Date & Time':
-            break
-        if not pd.isna(row[j]):
-            curr = matrix[df2_columns.index(j)]
-            if "-" in curr:
-                dash = curr.index('-')
-            else:
-                dash = len(curr)
-            sensorType = " ".join(curr[3:dash])
-            fieldID = curr[0]
-            sensorResult = row[j]
-            if dash != len(curr):
-                unit = curr[-1]
-            elif "State" in curr:
-                unit = 'ON/OFF'
-                if(row[j] == 'ON'):
-                    sensorResult = 1
+    print(df2[want[0]][4])
+    matrix = []
+    df2.replace(['--'], np.nan, inplace=True)
+    for i in df2.columns:
+        matrix.append(i.split())
+
+    for i in matrix:
+        if i[1].lower() != "port":
+            i[0] += i[1]
+            del i[1]
+        print(i, len(i))
+
+    df3 = pd.DataFrame(columns=['ID', 'Field ID', 'Date/Time', 'Sensor Type', 'Sensor Result', 'Unit'])
+
+    count = 0
+    track = 0
+    df2_columns = list(df2.columns)
+    for index, row in df2.iterrows():
+        for j in df2_columns:
+            print(track)
+            track += 1
+            if j == 'Date & Time':
+                break
+            if not pd.isna(row[j]):
+                curr = matrix[df2_columns.index(j)]
+                if "-" in curr:
+                    dash = curr.index('-')
                 else:
-                    sensorResult = 0
-            else:
-                unit = ""
-            new_row = pd.Series(data={'ID':count, 'Field ID':fieldID, 'Date/Time': row['Date & Time'], 'Sensor Type': sensorType, 'Sensor Result':sensorResult, 'Unit':unit})
-            df3  = df3.append(new_row, ignore_index = True)
-            count += 1
+                    dash = len(curr)
+                sensorType = " ".join(curr[3:dash])
+                fieldID = curr[0]
+                sensorResult = row[j]
+                if dash != len(curr):
+                    unit = curr[-1]
+                elif "State" in curr:
+                    unit = 'ON/OFF'
+                    if(row[j] == 'ON'):
+                        sensorResult = 1
+                    else:
+                        sensorResult = 0
+                else:
+                    unit = ""
+                new_row = pd.Series(data={'ID': count, 'Field ID': fieldID, 'Date/Time': row['Date & Time'], 'Sensor Type': sensorType, 'Sensor Result':sensorResult, 'Unit':unit})
+                df3 = df3.append(new_row, ignore_index=True)
+                count += 1
 
-df3.to_excel(r"C:\Users\Clyde\Desktop\SmartWash\sensors.xlsx")
+    df3.to_excel(r"C:\Users\Clyde\Desktop\SmartWash\sensors.xlsx")
 
 
 # RANCH WEATHER BELOW
 
-renamed[:24]
-want2 = renamed[:24]
-# want.append('Date & Time')
-df4= pd.DataFrame()
+def ranchWeather(path, end, csv):
+    if csv:
+        df = pd.read_csv(path, encoding='unicode_escape')
+    else:
+        df = pd.read_excel(path)
 
-for i in want2:
-    df4[i] = df[i]
+    renamed = []
+    for i in df:
+        renamed.append(i.strip())
 
-matrix2 = []
-df4.replace(['--'], np.nan, inplace=True)
-for i in df4.columns:
-    matrix2.append(i.split())
+    df.columns = renamed
+    renamed[:end]
+    want2 = renamed[:end]
+    # want.append('Date & Time')
+    df2 = pd.DataFrame()
 
-df5 = pd.DataFrame(columns = ['ID','Date/Time', 'Sensor Type', 'Sensor Result', 'Unit']);
+    for i in want2:
+        df2[i] = df[i]
 
-count2 = 0
-lol2 = 0
-df4_columns = list(df4.columns)
-for index, row in df4.iterrows():
-    for j in df4_columns:
-        print(lol2)
-        lol2+=1
-        if j == 'Date & Time':
-            continue
-        if not pd.isna(row[j]):
-            curr = matrix2[df4_columns.index(j)]
-            if "-" in curr:
-                dash = curr.index('-')
-            else:
-                dash = len(curr)
-            sensorType = " ".join(curr[0:dash])
-            print(sensorType)
-            sensorResult = row[j]
-            if dash != len(curr):
-                unit = curr[-1]
-            elif "State" in curr:
-                unit = 'ON/OFF'
-                if(row[j] == 'ON'):
-                    sensorResult = 1
+    matrix2 = []
+    df2.replace(['--'], np.nan, inplace=True)
+    for i in df2.columns:
+        matrix2.append(i.split())
+
+    df3 = pd.DataFrame(columns=['ID', 'Date/Time', 'Sensor Type', 'Sensor Result', 'Unit'])
+
+    count2 = 0
+    tracker = 0
+    df4_columns = list(df2.columns)
+    for index, row in df2.iterrows():
+        for j in df4_columns:
+            print(tracker)
+            tracker += 1
+            if j == 'Date & Time':
+                continue
+            if not pd.isna(row[j]):
+                curr = matrix2[df4_columns.index(j)]
+                if "-" in curr:
+                    dash = curr.index('-')
                 else:
-                    sensorResult = 0
-            else:
-                unit = ""
-            new_row = pd.Series(data={'ID':count2, 'Date/Time': row['Date & Time'], 'Sensor Type': sensorType, 'Sensor Result':sensorResult, 'Unit':unit})
-            df5  = df5.append(new_row, ignore_index = True)
-            count2 += 1
+                    dash = len(curr)
+                sensorType = " ".join(curr[0:dash])
+                print(sensorType)
+                sensorResult = row[j]
+                if dash != len(curr):
+                    unit = curr[-1]
+                elif "State" in curr:
+                    unit = 'ON/OFF'
+                    if(row[j] == 'ON'):
+                        sensorResult = 1
+                    else:
+                        sensorResult = 0
+                else:
+                    unit = ""
+                new_row = pd.Series(data={'ID': count2, 'Date/Time': row['Date & Time'], 'Sensor Type': sensorType, 'Sensor Result':sensorResult, 'Unit':unit})
+                df3 = df3.append(new_row, ignore_index=True)
+                count2 += 1
 
-df5.to_excel(r"C:\Users\csumagang\Desktop\SmartWash_2020\ranch_sensors2_new2.xlsx")
+    df3.to_excel(r"C:\Users\csumagang\Desktop\SmartWash_2020\ranch_sensors2_new2.xlsx")
 
 
 # SAMPLES BELOW
 
-df = pd.read_excel(r"C:\Users\csumagang\Desktop\SmartWash_2020\Database_Raw_RR_ResultsUploadTest.xlsx")
-df.shape
-df.columns
-df2 = pd.read_excel(r"C:\Users\csumagang\Desktop\tblFieldSample.xlsx")
+def createSamples(path, start):
+    df = pd.read_excel(r"C:\Users\csumagang\Desktop\SmartWash_2020\Database_Raw_RR_ResultsUploadTest.xlsx")
+    df.shape
+    df.columns
+    df2 = pd.read_excel(r"C:\Users\csumagang\Desktop\tblFieldSample.xlsx")
 
-df.rename(columns={"Sample Type": "SampleType", "Sample Subtype": "SampleSubType", "Sample Barcode": "Barcode", "Picture": "Media"}, inplace=True)
+    df.rename(columns={"Sample Type": "SampleType", "Sample Subtype": "SampleSubType", "Sample Barcode": "Barcode", "Picture": "Media"}, inplace=True)
 
-df3 = pd.DataFrame(columns = ['Lab', 'Method', 'Date', 'Time', 'SamplesID', 'Result'])
-offFieldDF = pd.DataFrame(columns = ['SampleType', 'SampleSubType', 'RawDate', 'SpecificSampler', 'GPS', 'Barcode', 'Media', 'Notes', 'ExperimentsID'])
-offFieldresDF = pd.DataFrame(columns = ['Lab', 'Method', 'Date', 'Time', 'SamplesID', 'Result'])
+    df3 = pd.DataFrame(columns = ['Lab', 'Method', 'Date', 'Time', 'SamplesID', 'Result'])
+    offFieldDF = pd.DataFrame(columns = ['SampleType', 'SampleSubType', 'RawDate', 'SpecificSampler', 'GPS', 'Barcode', 'Media', 'Notes', 'ExperimentsID'])
+    offFieldresDF = pd.DataFrame(columns = ['Lab', 'Method', 'Date', 'Time', 'SamplesID', 'Result'])
 
-methods = df.columns[17:]
-count = 0
-offCount = 1
+    methods = df.columns[17:]
+    count = 0
+    offCount = 1
 
-for index, row in df.iterrows():
-    print(count)
-    checker = False
-    for inner_index, inner_row in df2.iterrows():
-        if str(row['SampleType']) == str(inner_row['SampleType']) and str(row['SampleSubType']) == str(inner_row['SampleSubType']) and str(row['GPS']) == str(inner_row['GPS']) and str(row['Barcode']) == str(inner_row['Barcode']) and str(row['Media']) == str(inner_row['Media']) and str(row['Notes']) == str(inner_row['Notes']):
-            checker = True
+    for index, row in df.iterrows():
+        print(count)
+        checker = False
+        for inner_index, inner_row in df2.iterrows():
+            if str(row['SampleType']) == str(inner_row['SampleType']) and str(row['SampleSubType']) == str(inner_row['SampleSubType']) and str(row['GPS']) == str(inner_row['GPS']) and str(row['Barcode']) == str(inner_row['Barcode']) and str(row['Media']) == str(inner_row['Media']) and str(row['Notes']) == str(inner_row['Notes']):
+                checker = True
+                for i in methods:
+                    if pd.isnull(row[i]):
+                        continue
+                    else:
+                        new_row = pd.Series(data={'Lab': row['Lab'], 'Method': i, 'Date': row['Date'], 'Time': row['Time'], 'SamplesID': inner_row['SamplesID'], 'Result': row[i]})
+                        df3  = df3.append(new_row, ignore_index = True)
+        if checker == False:
+            new_row = pd.Series(data={'SampleType': row['SampleType'], 'SampleSubType': row['SampleSubType'], 'RawDate': row['Date'], 'SpecificSampler': 'Sullivan, Genevieve', 'GPS': row['GPS'], 'Barcode': row['Barcode'], 'Media': row['Media'], 'Notes': row['Notes']})
+            offFieldDF = offFieldDF.append(new_row, ignore_index = True)
             for i in methods:
                 if pd.isnull(row[i]):
                     continue
                 else:
-                    new_row = pd.Series(data={'Lab': row['Lab'], 'Method': i, 'Date': row['Date'], 'Time': row['Time'], 'SamplesID': inner_row['SamplesID'], 'Result': row[i]})
-                    df3  = df3.append(new_row, ignore_index = True)
-    if checker == False:
-        new_row = pd.Series(data={'SampleType': row['SampleType'], 'SampleSubType': row['SampleSubType'], 'RawDate': row['Date'], 'SpecificSampler': 'Sullivan, Genevieve', 'GPS': row['GPS'], 'Barcode': row['Barcode'], 'Media': row['Media'], 'Notes': row['Notes']})
-        offFieldDF = offFieldDF.append(new_row, ignore_index = True)
-        for i in methods:
-            if pd.isnull(row[i]):
-                continue
-            else:
-                new_row = pd.Series(data={'Lab': row['Lab'], 'Method': i, 'Date': row['Date'], 'Time': row['Time'], 'SamplesID': offCount, 'Result': row[i]})
-                offFieldresDF  = offFieldresDF.append(new_row, ignore_index = True)
-        offCount += 1
+                    new_row = pd.Series(data={'Lab': row['Lab'], 'Method': i, 'Date': row['Date'], 'Time': row['Time'], 'SamplesID': offCount, 'Result': row[i]})
+                    offFieldresDF = offFieldresDF.append(new_row, ignore_index = True)
+            offCount += 1
 
-    count+=1
+        count+=1
 
-df3.to_excel(r"C:\Users\csumagang\Desktop\SmartWash_2020\Jul7_New_ResultsData2.xlsx")
-offFieldDF.to_excel(r"C:\Users\csumagang\Desktop\SmartWash_2020\Jul_OffFieldSamp.xlsx")
-offFieldresDF.to_excel(r"C:\Users\csumagang\Desktop\SmartWash_2020\Jul_OffFieldRes.xlsx")
+    df3.to_excel(r"C:\Users\csumagang\Desktop\SmartWash_2020\Jul7_New_ResultsData2.xlsx")
+    offFieldDF.to_excel(r"C:\Users\csumagang\Desktop\SmartWash_2020\Jul_OffFieldSamp.xlsx")
+    offFieldresDF.to_excel(r"C:\Users\csumagang\Desktop\SmartWash_2020\Jul_OffFieldRes.xlsx")
 
 
 # GENERATE FIELD WEATHER LIST
-df3['Sensor Type'].unique()
-df_weather_sensors = pd.DataFrame();
-df_weather_sensors['FieldWeatherSensor'] = df3['Sensor Type'].unique
 
-df_weather_sensors.to_excel(r"C:\Users\csumagang\Desktop\SmartWash_2020\field_weather_sensor_list.xlsx")
+def generateFieldWeatherList():
+    df3['Sensor Type'].unique()
+    df_weather_sensors = pd.DataFrame();
+    df_weather_sensors['FieldWeatherSensor'] = df3['Sensor Type'].unique
+
+    df_weather_sensors.to_excel(r"C:\Users\csumagang\Desktop\SmartWash_2020\field_weather_sensor_list.xlsx")
+
 
 # GENERATE RANCH WEATHER LIST
-df5['Sensor Type'].unique()
-df_ranch_sensors = pd.DataFrame();
-df_ranch_sensors['RanchWeatherSensor'] = df5['Sensor Type'].unique()
 
-df_ranch_sensors.to_excel(r"C:\Users\csumagang\Desktop\SmartWash_2020\ranch_weather_sensor_list.xlsx")
+def generateRanchWeatherList():
+    df5['Sensor Type'].unique()
+    df_ranch_sensors = pd.DataFrame();
+    df_ranch_sensors['RanchWeatherSensor'] = df5['Sensor Type'].unique()
+
+    df_ranch_sensors.to_excel(r"C:\Users\csumagang\Desktop\SmartWash_2020\ranch_weather_sensor_list.xlsx")
--- sensorGUI.py
+++ sensorGUI.py
@@ -1,12 +1,13 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIntValidator
+from excelParser import *
 
 
 """
 TODO: Fix layout
-TODO: Connect to excel parser
 TODO: Compile and create executable
+TODO: Add drop down for field/ranch and sample
 """
 
 
@@ -14,7 +15,7 @@ class SensorGUI(QWidget):
     def __init__(self, parent = None):
         super(SensorGUI, self).__init__(parent)
         self.fname = ''
-        self.setFixedSize(640, 150)
+        self.setFixedSize(650, 250)
         self.initUI()
 
     def initUI(self):
@@ -26,27 +27,48 @@ class SensorGUI(QWidget):
 
         self.radiobutton = QRadioButton("CSV")
         self.radiobutton.type = "csv"
+        self.radiobutton.toggled.connect(self.onClicked)
         hbox.addWidget(self.radiobutton)
 
         self.radiobutton = QRadioButton("EXCEL")
+        self.radiobutton.setChecked(True)
         self.radiobutton.type = "excel"
+        self.radiobutton.toggled.connect(self.onClicked)
         hbox.addWidget(self.radiobutton)
 
         vbox.addLayout(hbox)
         self.fileName = QLabel("")
         vbox.addWidget(self.fileName)
 
+        self.combo = QComboBox()
+        self.combo.addItem("Field")
+        self.combo.addItem("Ranch")
+        self.combo.addItem("Both")
+        vbox.addWidget(self.combo)
+
         self.btn = QPushButton("Choose Sensor Excel File")
         self.btn.clicked.connect(self.getfile)
         vbox.addWidget(self.btn)
 
         self.contents = QLineEdit()
-        self.contents.setPlaceholderText("Enter Num of Columns for Field Weather")
+        self.contents.setPlaceholderText("Enter Start Column # for Field Weather")
         self.contents.setValidator(QIntValidator())
         vbox.addWidget(self.contents)
+
+        self.contents2 = QLineEdit()
+        self.contents2.setPlaceholderText("Enter End Column # for Ranch Weather")
+        self.contents2.setValidator(QIntValidator())
+        vbox.addWidget(self.contents2)
+
+        self.parseBtn = QPushButton("Parse Information")
+        self.parseBtn.clicked.connect(self.parseInfo)
+        vbox.addWidget(self.parseBtn)
+
         self.setLayout(vbox)
         self.setWindowTitle("Field Sensor Parser")
-        print("END")
+
+    def onClicked(self):
+        self.radiobutton = self.sender()
 
     def getfile(self):
         self.fname = QFileDialog.getOpenFileName(self)[0]
@@ -55,6 +77,24 @@ class SensorGUI(QWidget):
     def printer(self):
         self.fileName.setText(self.fname)
         print(self.fname)
+
+    def parseInfo(self):
+        print('starting Parse')
+        print(self.fname)
+        message = QMessageBox()
+        message.setWindowTitle("Current Status")
+        if str(self.combo.currentText()) == "Field" or str(self.combo.currentText()) == "Both":
+            message.setText("Currently Processing Field Weather. Do not exit!")
+            message.exec_()
+            fieldWeather(self.fname, self.contents, self.radiobutton.type)
+        if str(self.combo.currentText()) == "Ranch" or str(self.combo.currentText()) == "Both":
+            message.setText("Currently Processing Ranch Weather. Do not exit!")
+            message.exec_()
+            ranchWeather(self.fname, self.contents2, self.radiobutton.type)
+        message.setText("Finished! You may exit!")
+        message.exec_()
+        print('finished Parse')
+
 
 def main():
     app = QApplication(sys.argv)
